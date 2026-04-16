e9dbddef6cd685b8e518cd9e0b9f939b0ffed827
Florian <costes.fl@gmail.com>
2026-04-16 23:27:20 +0200

    add link to art and appendchild - add slug - add css recto-verso card - add appendchild card

--- js/articles.js
+++ js/articles.js
@@ -1,6 +1,7 @@
 const message = document.querySelector("#message")
 const cardArt = document.querySelector("#cardArt")
-
+const URL = "../data/articles.json"
+const URLart = "../article-single.html"
 //1. créer une fonction pour charger les articles  
 // (récupérer avec try...catch)
 //2. fetch pour chercher les articles  
@@ -8,15 +9,15 @@ const cardArt = document.querySelector("#cardArt")
 //4. créer les balises html en js  
 //5. injecter dans les balises html de l'étape 4
 //6. injecter les balises dans le html
-//7. déclencher le chargement avec le bouton en utilisant addEventListener
 
 async function chargerArticles() {
     console.log("Chargement en cours...")
 
     try {
-        const response = await fetch("../data/articles.json")
+        const response = await fetch(URL)
         if (!response.ok) {
             throw new Error("Error: " + response.status)
+            // throw sort du TRy pour aller vers CATCH et pour s'occuper de l'erreur
         }
 
         const articles = await response.json()
@@ -26,6 +27,12 @@ async function chargerArticles() {
         articles.forEach(article => {
             const card = document.createElement("div")
             card.classList.add("card")
+
+            // ===========  CRREATION CARTE RECTO =========== 
+
+            const recto = document.createElement("div")
+            recto.classList.add("card-recto")
+
             // ===========  ELEMENTS =========== 
             const title = document.createElement("h3")
             title.textContent = article.title
@@ -40,44 +47,83 @@ async function chargerArticles() {
 
             const readingTime = document.createElement("p")
             readingTime.textContent = article.readingTime
+
             const imgReadingTime = document.createElement("img")
             imgReadingTime.src = ("../assets/icons/readTime.svg")
 
 
-            const summary = document.createElement("p")
-            summary.textContent = article.summary
             // ===========  INFOS METADONNE ART =========== 
-            const metaCard = document.createElement("div")
-            metaCard.classList.add("metaCard")
-
-            const createdAt = document.createElement("p")
-            createdAt.textContent = article.createdAt
+            const metaCardRecto = document.createElement("div")
+            metaCardRecto.classList.add("metaCard")
 
             const category = document.createElement("p")
             category.textContent = article.category
 
+            const imgFavori = document.createElement("img")
+            imgFavori.src = ("../assets/icons/plus.svg")
+
+
+
+            //==============================================
+            // ===========  CRREATION CARTE VERSO =========== 
+
+            const verso = document.createElement("div")
+            verso.classList.add("card-verso")
+
+            // ===========  ELEMENTS =========== 
+            const summary = document.createElement("p")
+            summary.textContent = article.summary
+
+            // ===========  INFOS METADONNE ART =========== 
+            const metaCardVerso = document.createElement("div")
+            metaCardVerso.classList.add("metaCard")
+
+            const createdAt = document.createElement("p")
+            createdAt.textContent = article.createdAt
+
             const author = document.createElement("p")
             author.textContent = article.author
 
-            card.appendChild(readingTimeDIV)
-            card.appendChild(img)
-            card.appendChild(title)
-            card.appendChild(summary)
-            card.appendChild(metaCard)
+            const btnReadArt = document.createElement("button")
+            btnReadArt.textContent = "Lire l'article"
 
-            metaCard.appendChild(createdAt)
-            metaCard.appendChild(category)
-            metaCard.appendChild(author)
+
+            card.appendChild(recto)
+            card.appendChild(verso)
+
+            recto.appendChild(readingTimeDIV)
+            recto.appendChild(img)
+            recto.appendChild(title)
+            recto.appendChild(metaCardRecto)
+
+            metaCardRecto.appendChild(category)
+            metaCardRecto.appendChild(imgFavori)
 
             readingTimeDIV.appendChild(readingTime)
             readingTimeDIV.appendChild(imgReadingTime)
+
+            verso.appendChild(summary)
+            verso.appendChild(metaCardVerso)
+
+            metaCardVerso.appendChild(createdAt)
+            metaCardVerso.appendChild(author)
+            metaCardVerso.appendChild(btnReadArt)
 
 
             cardArt.appendChild(card)
 
             card.addEventListener('click', () => {
+                card.classList.toggle("flipped")
+            })
+            title.addEventListener('click', (event) => {
+                event.stopPropagation()
                 localStorage.setItem("articleSelectionne", JSON.stringify(article))
-                window.location.href = "../article-single.html"
+                window.location.href = `${URLart}?slug=${article.slug}`
+            })
+            btnReadArt.addEventListener('click', (event) => {
+                event.stopPropagation()
+                localStorage.setItem("articleSelectionne", JSON.stringify(article))
+                window.location.href = `${URLart}?slug=${article.slug}`
             })
 
         });
--- js/singleArt.js
+++ js/singleArt.js
@@ -1,10 +1,81 @@
 const conteneurArt = document.querySelector("#conteneurArt")
-const artDetail = document.querySelector("#artDetail")
+const params = new URLSearchParams(window.location.search)
+const slug = params.get("slug")
+const URL = "../data/articles.json"
 
-const article = JSON.parse(localStorage.getItem("articleSelectionne"))
-console.log(article)
+async function afficherArticle() {
+    console.log("chargement de l'article")
+
+    try {
+        const response = await fetch(URL)
+        if (!response.ok) {
+            throw new Error("Error" + response.status)
+        }
+
+        const articles = await response.json()
+        console.log("article affiché avec succès")
+
+        const article = articles.find(a => a.slug === slug)
+
+        const title = document.createElement("h1")
+        title.textContent = article.title
+
+        const hr = document.createElement("hr")
+        hr.classList.add("hr")
+
+        // ===========  INFOS METADONNE ART =========== 
+        const metaDiv = document.createElement("div")
+        metaDiv.classList.add("metaCard")
+
+        const imgFavori = document.createElement("img")
+        imgFavori.src = ("../assets/icons/plus.svg")
+
+        const createdAt = document.createElement("p")
+        createdAt.textContent = article.createdAt
+
+        const category = document.createElement("p")
+        category.textContent = article.category
+
+        const author = document.createElement("p")
+        author.textContent = article.author
+
+        // ===========  ELEMENTS =========== 
+        const bodyArt = document.createElement("div")
+
+        const img = document.createElement("img")
+        img.src = article.image
+        img.alt = `image de l'arcticle ${title}`
+        img.classList.add("card-image")
+
+        const summary = document.createElement("p")
+        summary.textContent = article.summary
+
+        const content = document.createElement("p")
+        content.textContent = article.content
 
 
-const title = document.createElement("h1")
-title.textContent = article.title
-artDetail.appendChild(title)
+        conteneurArt.appendChild(title)
+        conteneurArt.appendChild(hr)
+        conteneurArt.appendChild(metaDiv)
+        conteneurArt.appendChild(bodyArt)
+
+        metaDiv.appendChild(imgFavori)
+        metaDiv.appendChild(createdAt)
+        metaDiv.appendChild(category)
+        metaDiv.appendChild(author)
+
+        bodyArt.appendChild(img)
+        bodyArt.appendChild(summary)
+        bodyArt.appendChild(content)
+
+
+    } catch (Error) {
+        console.log("Une erreur est survenue lors du chargement de l'article " + Error.message)
+    }
+}
+afficherArticle()
+
+console.log(slug)
+
+
+
